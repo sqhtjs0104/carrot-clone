e4e163de9da07f88f235ed4f17ed0a5ca9b807dd
BOSEON <sqhtjs0104@gmail.com>
2024-08-17 18:33:14 +0900

    Infinity scrolling

--- components/list-product.tsx
+++ components/list-product.tsx
@@ -20,7 +20,7 @@ export default function ListProduct({
   return (
     <Link href={`/products/${id}`} className="flex gap-5">
       <div className="relative size-28 rounded-md overflow-hidden">
-        <Image src={photo} alt={title} fill />
+        <Image src={photo} alt={title} fill className="object-cover" />
       </div>
       <div className="flex flex-col gap-1 *:text-white">
         <span className="text-lg">{title}</span>
